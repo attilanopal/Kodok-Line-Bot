f1b607f74fadd22c25611b058cad3b5b6ad13f8e
Attila <attilanfl14@gmail.com>
2020-12-25 19:33:39 +0800

    Reply Terakhirr

--- public/index.php
+++ public/index.php
@@ -61,23 +61,18 @@ if(is_array($data['events'])){
             if($event['message']['type'] == 'text')
             {
                 // send same message as reply to user
-                $textMessageBuilder1 = new TextMessageBuilder('ini pesan balasan pertama');
-                $textMessageBuilder2 = new TextMessageBuilder('ini pesan balasan kedua');
-                $stickerMessageBuilder = new StickerMessageBuilder(1, 106);
-            
-                $multiMessageBuilder = new MultiMessageBuilder();
-                $multiMessageBuilder->add($textMessageBuilder1);
-                $multiMessageBuilder->add($textMessageBuilder2);
-                $multiMessageBuilder->add($stickerMessageBuilder);
+                $result = $bot->replyText($event['replyToken'], $event['message']['text']);
 
-                $result = $bot->replyMessage($event['replyToken'], $multiMessageBuilder);
-                
- 
+
+
+
                 // or we can use replyMessage() instead to send reply message
                 // $textMessageBuilder = new TextMessageBuilder($event['message']['text']);
                 // $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
- 
- 
+
+
+
+
                 $response->getBody()->write(json_encode($result->getJSONDecodedBody()));
                 return $response
                     ->withHeader('Content-Type', 'application/json')
@@ -85,7 +80,9 @@ if(is_array($data['events'])){
             }
         }
     }
-} 
+    return $response->withStatus(200, 'for Webhook!'); //buat ngasih response 200 ke pas verify webhook
+}
+return $response->withStatus(400, 'No event sent!');
 });
 $app->run();
  
