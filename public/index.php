dd781d303a3bbe8c28beb11e57fbfaa3d66fa5c3
Attila <attilanfl14@gmail.com>
2020-12-26 12:47:46 +0800

    test var

--- public/index.php
+++ public/index.php
@@ -20,9 +20,7 @@ use \LINE\LINEBot\SignatureValidator as SignatureValidator;
  
 $pass_signature = true;
 
-// Variabel buatan Atl Ngokhey
-$autoReplyStatus = '0ff';
-$prefix='!';
+
  
 // set LINE channel_access_token and channel_secret
 $channel_access_token = "B9h0QfSxK3g6cDGBE9YnTKPqF3N31qP2f+crDf5Qi4UzUnoQzkUYP4qArMseGRxSkv/tep7B78zqLeitmUz3KvsSJNQMRa3QjLUsaBmjqAv9bf0nSd39KWrDAEqOxEiqr5n/mwOu6x2fF5jAYk+RqAdB04t89/1O/w1cDnyilFU=";
@@ -71,7 +69,9 @@ $app->post('/webhook', function (Request $request, Response $response) use ($cha
             return $response->withStatus(400, 'Invalid signature');
         }
     }
- 
+    // Variabel buatan Atl Ngokhey
+    $autoReplyStatus = 'Off';
+    $prefix='!';
  
     $data = json_decode($body, true);
     if(is_array($data['events'])){
