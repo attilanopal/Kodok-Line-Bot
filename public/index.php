d7cb6d5d820c4c28d1316c55df99ad350341c63e
Attila <attilanfl14@gmail.com>
2020-12-26 12:15:39 +0800

    GetUserId Workkk

--- public/index.php
+++ public/index.php
@@ -79,7 +79,7 @@ $app->post('/webhook', function (Request $request, Response $response) use ($cha
                 if($event['message']['type'] == 'text')
                 {
                     // send same message as reply to user
-                    $result = $bot->replyText($event['replyToken'],$event['source']['userId']);
+                    $result = $bot->replyText($event['replyToken'],"ini user Id kamu :) :".$event['source']['userId']);
                     
  
  
